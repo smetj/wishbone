f8872578d20e0b1416ace79f2186e83cafa6bee9
Jelle Smet <github@smetj.net>
2012-04-18 00:25:04 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#  
+#  Copyright 2012 Jelle Smet development@smetj.net
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+from distutils.core import setup
+
+
+if __name__ == '__main__':
+    setup(name='wishbone',
+        version='0.1',
+        description='A python module which facilitates writing modular message passing code based on gevent.',
+        author='Jelle Smet',
+        author_email='development@smetj.net',
+        url='https://github.com/smetj/wishbone',
+        packages=['io','modules'])
