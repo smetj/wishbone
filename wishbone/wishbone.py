ec1b178fcf009274d9865172ef7c3465dfe4ee7c
Jelle Smet <git@smetj.net>
2012-04-27 22:16:26 +0200

    adding some more help

--- wishbone/wishbone.py
+++ wishbone/wishbone.py
@@ -28,17 +28,43 @@ from gevent import sleep, spawn
 from gevent.queue import Queue
 
 class Wishbone():
+    '''
+    Wishbone is a module which allows you to write gevent based, modular message passing code.
+    
+    Wishbone allows you to build a data flow by connecting the in- and outbox queue of multiple modules.
+    The goal is that each module has (at least) an inbox and outbox queue.  Each module has a consume() function,
+    which is called upon to all messages from its inbox queue.  It's up to the consume() function to put the data
+    into its outbox queue.  A programmer can then connect the outbox of one module to the inbox of another in order
+    to create a dataflow.  The result is that one is able to create code which is modular and almost "graphical" because
+    like this it is very easy to define processing blocks and dataflow.    
+    '''
     
     def __init__(self):
         self.lock=True
         self.__configureLogging()
         self.logging = logging.getLogger( 'Wishbone' )
         self.modules=[]
-        self.servers=[]
         self.hub = Queue(None)
         self.outhub = Queue(None)
         
     def registerModule(self, config, *args, **kwargs):
+        '''Registers a Wishbone Module into the framework.  All modules used within Wishbone should be regesitered through this function.
+        
+        This function receives a tuple containing 3 values.  Any further args or kwargs are used to initialize the actual module you register.
+        
+        The config parameter should be a tuple of 3 strings:
+            
+            (module, class, name)
+        
+            module:     The name of the module to import.
+            class:      The name of the class to initialize
+            name:       The name under which the module should be initialized.
+            
+            *args and **kwargs are passed to the class which is initialized.
+        
+        self.modules contains a list of all registered modules.  Also, each registered module is registered under self.name, where name is last
+        value of the tuple.'''
+               
         module_name = config[0]
         class_name = config[1]
         name = config[2]        
@@ -49,10 +75,23 @@ class Wishbone():
         except Exception as err:
             print "Problem loading module: %s and class %s. Reason: %s" % ( module_name, class_name, err)
         
-    def connect(self,inbox,outbox):
-        spawn ( self.__connector, inbox, outbox )
+    def connect(self, source, destination):
+        '''Creates a new background Greenthread which continuously consumes all messages from source into destination.
+        
+        This function is used to connect the queue of one module with the queue of another.  A typical useage is to connect outbox from module1
+        to inbox of module2.
+        '''
+        
+        spawn ( self.__connector, source, destination )
     
     def start(self):
+        '''Function which starts all registered Wishbone modules.
+        
+        Function which runs over all registered instances/modules and tries to execute the start() function in order to let that module start
+        to consume the messages of its inbox and execute the consume function on each message.
+        This function blocks from exiting.
+        '''
+        
         for instance in self.__dict__:
             try:
                 self.__dict__[instance].start()
@@ -62,6 +101,11 @@ class Wishbone():
             sleep(1)
     
     def stop(self):
+        '''Function which stops all registered Wishbone modules.
+        
+        Function which runs over all registered instances/modules and tries to execute its stop() function in order to stop that module.
+        '''
+        
         self.lock=False
         for instance in self.__dict__:
             try:
@@ -70,14 +114,23 @@ class Wishbone():
                 pass
     
     def block(self):
+        '''Function which provides a global block where required.'''
+        
         return self.lock
          
     def __connector(self,source, destination):
         '''Consumes data from source and puts it in destination.'''
+        
         while self.block() == True:
             destination.put(source.get())
         
     def __configureLogging(self,syslog=False,loglevel=logging.INFO):
+        '''Configures logging.
+        
+        Configures the format of the logging messages.  This function accepts 1 parameter:
+        
+        loglevel: defines the loglevel.'''
+        
         format=('%(asctime)s %(levelname)s %(name)s: %(message)s')
         if syslog == False:
             logging.basicConfig(level=loglevel, format=format)
