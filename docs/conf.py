d379c4a3d8b2cf59f128b4634d699bc8e08639c3
Jelle Smet <development@smetj.net>
2012-05-04 23:50:46 +0200

    readthedocsshizz

--- docs/conf.py
+++ docs/conf.py
@@ -261,6 +261,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['gevent','gevent.queue','amqplib','snappy']
+MOCK_MODULES = ['gevent','gevent.queue','gevent.server','amqplib','snappy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
