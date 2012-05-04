fb81c0d2bdbfbd5727a6a10330cf33dd3edcc56b
Jelle Smet <development@smetj.net>
2012-05-04 23:31:30 +0200

    docs

--- wishbone/modules/jsonvalidator.py
+++ wishbone/modules/jsonvalidator.py
@@ -29,10 +29,12 @@ from wishbone.toolkit import PrimitiveActor
 class JSONValidator(PrimitiveActor):
     '''A WishBone module which verifies JSON data against a validator schema loaded from file.
     
+
     Messages consumed from the inbox queue is verified against a Validator schema.  When the message is not a valid JSON document
     or when it doesn't match your predifined Validator schema, it is dropped.
     This module accepts 2 parameters:
         
+
         schema:     The location and filename of the schema to load.  The schema should follow http://json-schema.org/ specifications.
         convert:    When True it will aditionally convert the incoming JSON string to a Python object.
     '''    
