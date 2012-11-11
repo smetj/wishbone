a2ecb7f9873723f32e4e6320d9ff1ec54343104d
Jelle Smet <development@smetj.net>
2012-11-11 23:10:53 +0100

    made both modules a bit nicer.

--- wishbone/modules/tippingbucket.py
+++ wishbone/modules/tippingbucket.py
@@ -2,94 +2,127 @@
 # -*- coding: utf-8 -*-
 #
 #       tippingbucket.py
-#       
+#
 #       Copyright 2012 Jelle Smet development@smetj.net
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
-#       
-#       
+#
+#
 
 from wishbone.toolkit import PrimitiveActor
 from time import time
 from gevent import sleep, spawn
 from gevent.event import Event
-import ast
+from gevent import monkey;monkey.patch_all()
 
 class TippingBucket(PrimitiveActor):
-    '''Buffers data and dumps to the output on 2 conditions:
-        
+    
+    '''Buffers unicode data and dumps to the output on 2 conditions:
+
         * Last data enteered in buffer is older than x seconds.
         * Total data in buffer is x size.
+
+    When the buffer is empty, the header of the first incoming message will be used as the header
+    for the message going out containing the content of the buffer.  If you want to override that
+    header with a predefined one then use the <predefined_header> option.
     
-    The messages with the same header are buffered together.        
     Realize this is a buffer PER WishBone instance.
 
     Parameters:
-    
+
         * age:  The time in seconds since the first update when the buffer is flushed.
                 Default: 10
                 Type: Int
-                
-        * size: The total size of the buffer when it is flushed.
+
+        * size: The total size in bytes of the buffer when it is flushed.
                 Default: 10000
                 Type: Int
-    '''
-    
-    def __init__(self, name, *args, **kwargs):
         
+        * predefined_header:    Once the buffer is flushed the message containing the content of the
+                                buffer will have this header instead of the header coming from the
+                                first package entering the buffer.
+    
+    Disclaimer: The size of the buffer is expressed in bytes.  This is *only* valid when you use
+                "single-byte encoding" characters.  Even then it's going to be an approximation.
+                When unicode is used then this counter is not going to be realistic anymore.
+                
+    Disclaimer2:    Binary data is not going to work.  If you require that, encode into base64 or
+                    something similar.  In that case take the growth of that into account:
+                    
+                        ~ output_size = ((input_size - 1) / 3) * 4 + 4
+                    
+                    So 256 bytes binary data results into 344 bytes of Base64 data.
+    
+    ToDo(smetj):    Check for each incoming string which type of encoding we're talking about
+                    and take into account when "calculating" the total size of the buffer.
+                
+    '''
+
+    def __init__(self, name, *args, **kwargs):
+
         PrimitiveActor.__init__(self, name)
         self.age = kwargs.get('age',5)
         self.size = kwargs.get('size',10000)
-        self.buff={}
+        self.predefined_header=kwargs.get('predefined_header',None)
+        self.buff=[]
+        self.buff_age=0
+        self.buff_size=0
+        self.buff_header={}
         spawn(self.reaper)
-        
-    def consume(self,doc):
-        key = str(doc["header"])
 
-        if not key in self.buff:
-            self.buff[key]={"length":0,"last":0,"data":[]}
-            self.buff[key]["last"]=time()            
-        
-        self.buff[key]["data"].append(doc["data"])
-        self.buff[key]["length"]+=len(str(doc["data"]))
-        
-        if self.buff[key]["length"] > self.size:
-            self.logging.info("Size of buffer %s exceeded %s. Flushed."%(key,self.size))
-            self.dumpPart(key)
-    
+    def consume(self,doc):
+
+        if self.buff_age==0:
+            self.buff_age=time()
+            self.header=doc["header"]
+
+        self.buff.append(doc["data"])
+        self.buff_size+=len((doc["data"]))
+
+        if self.buff_size > self.size:
+            self.logging.info("Size of buffer exceeded. Flushed.")
+            self.flushBuffer()
+
     def reaper(self):
-        '''Run over the dictionary every x seconds to see whether we have expired cache which needs to be dumped.'''
-        
+        '''Check whether our cache is expired and flush the buffer if its the case.'''
+
         while self.block():
-            items=[]
-            for item in self.buff:
-                if (float(time()) - float(self.buff[item]["last"])) > float(self.age):
-                    self.logging.info("Age of buffer %s exceeded %s seconds. Flushed."%(item,self.age))
-                    items.append(item)
-            
-            for item in items:
-                self.dumpPart(item)
+            if self.buff_age != 0:
+                if (float(time()) - float(self.buff_age)) > float(self.age):
+                    self.flushBuffer()
+                    self.logging.info("Age of buffer exceeded %s seconds. Flushed."%(self.age))
             sleep(0.1)
-            
-    def dumpPart(self, key):
-        '''Dumps the expired cache into the queue and deletes the key from the cache dictonary.'''
+
+    def flushBuffer(self):
+        '''Flushes the buffer.'''
+
+        if self.predefined_header == None:
+            self.sendData({"header":self.buff_header, "data":self.buff})
+        else:
+            self.sendData({"header":self.predefined_header, "data":self.buff})
+        self.resetBuffer()
+        sleep(0)
+
+    def resetBuffer(self):
+        '''Resets the counters.'''
         
-        header = ast.literal_eval(key)
-        self.sendData({"header":header,"data":self.buff[key]["data"]})
-        del(self.buff[key])
-    
+        self.buff=[]
+        self.buff_age=0
+        self.buff_size=0
+        self.buff_header={}
+
     def shutdown(self):
         self.logging.info('Shutdown')
