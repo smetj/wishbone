e8126f6de2c7bb16a8af81221660107454ef57d5
Jelle Smet <development@smetj.net>
2012-12-05 00:24:09 +0100

    documentation and cleanups

--- wishbone/modules/brokerheader.py
+++ wishbone/modules/brokerheader.py
@@ -1,62 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#       stdout.py
-#       
+#       brokerheader.py
+#
 #       Copyright 2012 Jelle Smet development@smetj.net
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
-#       
-#       
+#
+#
 
 from wishbone.toolkit import PrimitiveActor
 
 
-class STDOUT(PrimitiveActor):
-    '''**The STDOUT Wishbone module is a minimal module which prints each incoming
-    event to STDOUT**
-    
-    After printing the content of the events to STDOUT they are put into the outbox
-    queue unless otherwise defined using the purge parameter.  When the complete
-    parameter is True, the complete event is printed to STDOUT.  This module should
-    only be used for testing or demonstration purposes.    
-    
+class BrokerHeader(PrimitiveActor):
+    '''**A Wishbone module which adds a wishbone.io_modules.broker header to each
+    event**.
+
+    When events go to the outside world via the boker module, it must know which
+    exchange and routing key to use.  Typically one would connect this outbox queue
+    to the outboxqueue of the wishbone.io_modules.broker instance.
+
     Parameters:
-    
-        - complete (bool):  When True, print the complete event including headers.
-        - purge (bool):     When True the message is dropped and not put in outbox.
-    
+
+        - name (str):       The instance name when initiated.
+        - key (str):        The routing key to use.
+        - exchange (str):   The exchange to use.
+
     Queues:
-    
+
         - inbox:    Incoming events.
         - outbox:   Outgoing events.
     '''
-    
-    def __init__(self, name, complete=False, purge=False):
+
+    def __init__(self, name, key='wishbone', exchange=''):
+
         PrimitiveActor.__init__(self, name)
-        self.complete=complete
-        self.purge=purge
-        
+        self.key = key
+        self.exchange = exchange
+
     def consume(self,doc):
-        if self.complete == False:
-            print doc['data']
-        else:
-            print doc
-        if self.purge == False:
-            self.putData(doc)
-       
+
+        doc['header']['broker_key']=self.key
+        doc['header']['broker_exchange']=self.exchange
+        self.sendData(doc)
+
     def shutdown(self):
+
         self.logging.info('Shutdown')
--- wishbone/modules/tippingbucket.py
+++ wishbone/modules/tippingbucket.py
@@ -30,7 +30,9 @@ from gevent import monkey;monkey.patch_all()
 
 class TippingBucket(PrimitiveActor):
     
-    '''Buffers unicode data and dumps to the output on 2 conditions:
+    '''**TippingBucket is a Wishbone module which buffers data.**
+    
+    This module buffers data and dumps it to the output queue on 2 conditions:
 
         * Last data enteered in buffer is older than x seconds.
         * Total data in buffer is x size.
@@ -39,36 +41,29 @@ class TippingBucket(PrimitiveActor):
     for the message going out containing the content of the buffer.  If you want to override that
     header with a predefined one then use the <predefined_header> option.
     
-    Realize this is a buffer PER WishBone instance.
-
-    Parameters:
-
-        * age:  The time in seconds since the first update when the buffer is flushed.
-                Default: 10
-                Type: Int
-
-        * size: The total size in bytes of the buffer when it is flushed.
-                Default: 10000
-                Type: Int
-        
-        * predefined_header:    Once the buffer is flushed the message containing the content of the
-                                buffer will have this header instead of the header coming from the
-                                first package entering the buffer.
-    
     Disclaimer: The size of the buffer is expressed in bytes.  This is *only* valid when you use
                 "single-byte encoding" characters.  Even then it's going to be an approximation.
-                When unicode is used then this counter is not going to be realistic anymore.
+                When unicode is used this counter is not going to be realistic anymore.
                 
-    Disclaimer2:    Binary data is not going to work.  If you require that, encode into base64 or
+    Disclaimer2:    If you require binary data, encode into base64 or
                     something similar.  In that case take the growth of that into account:
                     
                         ~ output_size = ((input_size - 1) / 3) * 4 + 4
                     
                     So 256 bytes binary data results into 344 bytes of Base64 data.
     
-    ToDo(smetj):    Check for each incoming string which type of encoding we're talking about
-                    and take into account when "calculating" the total size of the buffer.
-                
+    Parameters:
+
+        - age (int):                The time in seconds since the first update when the buffer is flushed.
+        - size (int):               The total size in bytes of the buffer when it is flushed.
+        - predefined_header (dict): Assign this header to the buffered event when submitting to outbox.
+    
+    Queues:
+    
+        - inbox:    Incoming events.
+        - outbox:   Outgoing events.
+    
+    Todo(smetj): Figure out something smarter to find out the size of the buffer.                
     '''
 
     def __init__(self, name, age=5, size=10000, predefined_header=None):
