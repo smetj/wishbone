cf967d4a58054ad840b0515415fe2520dda60711
Jelle Smet <development@smetj.net>
2012-12-05 18:34:49 +0100

    reorg docs for gh-pages

--- wishbone/io_modules/domainsocket.py
+++ wishbone/io_modules/domainsocket.py
@@ -22,6 +22,7 @@
 #  
 #  
 
+import logging
 from os import remove
 from gevent.server import StreamServer
 from gevent import Greenlet, socket, sleep
@@ -33,35 +34,45 @@ class DomainSocket(Greenlet, QueueFunctions, Block):
     '''**A Wishbone IO module which accepts external input from a unix domain socket.**
     
     Creates a Unix domain socket to which data can be submitted.
+    The listener can run in 2 modes:
+    
+    - line: Each newline creates a Wishbone event.
+    - blob: Data comes in as 1 event untill client disconnects.
         
     Parameters:
 
-        - name (str):  The instance name when initiated.
-        - path (str):  The absolute path of the socket.
+        - name (str):   The instance name when initiated.
+        - path (str):   The absolute path of the socket.
+        - mode (str):   The mode UDP listener is in.  [ line, blob ]
     
     Queues:
     
-        - inbox:       Data coming from the outside world.
+        - inbox:       Data coming from the outside world.        
     ''' 
    
-    def __init__(self, name, path):
+    def __init__(self, name, path, mode="line"):
         Greenlet.__init__(self)
         QueueFunctions.__init__(self)
         Block.__init__(self)
         self.name=name
         self.logging = logging.getLogger( name )
         self.path = path
+        self.mode = mode
         self.sock = None
         self.__setup()
-        self.logging.info('Initialiazed.')
+        self.logging.info('Initialiazed in %s mode'%self.mode)
 
     def __setup(self):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.setblocking(0)
         self.sock.bind(self.path)
         self.sock.listen(50)
+        if self.mode == "line":
+            self.handle=self.__doLine
+        elif self.mode == "blob":
+            self.handle=self.__doBlob    
     
-    def handle(self, socket, address):
+    def __doLine(self, socket, address):
         '''Is called upon each incoming message, makes sure the data has the right Wishbone format and writes the it into self.inbox'''
         
         fileobj = socket.makefile()
@@ -75,6 +86,15 @@ class DomainSocket(Greenlet, QueueFunctions, Block):
                 self.putData({'header':{},'data':line.rstrip("\n")}, queue='inbox')
             sleep(0)
         fileobj.close()
+    
+    def __doBlob(self, socket, address):
+        '''Is called upon each incoming message, makes sure the data has the right Wishbone format and writes the it into self.inbox'''
+        
+        fileobj = socket.makefile()
+        blob = fileobj.readlines()
+        fileobj.close()
+        self.logging.debug ('Data received from %s' % (address) )     
+        self.putData({'header':{},'data':"".join(blob)}, queue='inbox')        
          
     def _run(self):
         try:
