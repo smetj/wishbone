fbbcf47c3b4f560e5fd8a34a5a11095df0401f2f
Jelle Smet <jelle.smet@tomtom.com>
2013-04-01 15:38:21 +0200

    conditional dep

--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 #  setup.py
-#  
+#
 #  Copyright 2013 Jelle Smet <development@smetj.net>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 PROJECT = 'wishbone'
 
@@ -33,6 +33,13 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 import os
 import sys
+
+install_requires=['gevent','python-daemon','egenix-mx-base','argparse']
+
+try:
+    import importlib
+except ImportError:
+    install_requires.append('importlib')
 
 try:
     long_description = open('README.rst', 'rt').read()
@@ -65,8 +72,8 @@ setup(
     scripts=[],
 
     provides=[],
-    dependency_links = ['http://github.com/SiteSupport/gevent/tarball/master#egg=gevent-1.0rc1'],
-    install_requires=['gevent','python-daemon','egenix-mx-base','argparse','importlib'],
+    dependency_links=['http://github.com/SiteSupport/gevent/tarball/master#egg=gevent-1.0rc1'],
+    install_requires=install_requires,
     namespace_packages=[],
     packages=find_packages(),
     include_package_data=True,
