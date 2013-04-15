84fe134101e47a21e7c7ffde9f5a398b6b8d0eb2
Jelle Smet <jelle.smet@tomtom.com>
2013-04-15 23:03:55 +0200

    adding dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from fnmatch import fnmatchcase
 import os
 import sys
 
-install_requires=['gevent>=1.0dev','python-daemon','egenix-mx-base','argparse','wb_log','greenlet>=0.3.2','jsonschema']
+install_requires=['gevent>=1.0dev','python-daemon','egenix-mx-base','argparse','wb_log','greenlet>=0.3.2','jsonschema','prettytable']
 
 try:
     import importlib
