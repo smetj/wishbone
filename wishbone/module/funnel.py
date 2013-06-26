5e82b7061878496d2604fa1a75a08fdee1f8a30b
Jelle Smet <development@smetj.net>
2013-06-26 22:47:02 +0200

    Adding funnel module

---
+++ wishbone/module/funnel.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+#  funel.py
+#
+#  Copyright 2013 Jelle Smet <development@smetj.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+from wishbone import Actor
+
+
+class Funnel(Actor):
+    '''**A builtin Wishbone module which merges incoming events from different
+    queues into 1 queue.**
+
+    Create a "n to 1" relationship with queues.  Events arriving in different
+    queues are all merged into the outbox.
+
+    Parameters:
+
+        name(str):  The name of the module
+
+    Queues:
+
+        outbox:     Outgoing events.
+
+    '''
+
+    def __init__(self, name):
+        Actor.__init__(self, name, limit=0)
+
+    def preHook(self):
+        source_queues = self.queuepool.getQueueInstances()
+        del(source_queues["inbox"])
+        del(source_queues["outbox"])
+        self.source_queues = [source_queues[queue] for queue in source_queues.keys()]
+        for queue in self.source_queues:
+            self.registerConsumer(self.consume, queue)
+
+    def consume(self, event):
+        self.queuepool.outbox.put(event)
