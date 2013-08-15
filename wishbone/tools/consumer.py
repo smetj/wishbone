f5b8c799bdf22930c0695edaa6aa4e9a53f49edf
Jelle Smet <development@smetj.net>
2013-08-15 18:14:38 +0200

    Set debug level.

--- wishbone/tools/consumer.py
+++ wishbone/tools/consumer.py
@@ -55,7 +55,7 @@ class Consumer(LoopContextSwitcher):
 
         for c in self.__doConsumes:
             self.__greenlet.append(spawn(self.__doConsume, c[0], c[1]))
-            self.logging.info('Function %s started to consume queue %s.'%(str(c[0]),str(c[1])))
+            self.logging.debug('Function %s started to consume queue %s.'%(str(c[0]),str(c[1])))
 
     def shutdown(self):
         '''Stops each module by making <self.loop> return False and which unblocks <self.block>'''
