1800723a2ec119d434ab0a1c06b5da109e8d31c8
Jelle Smet <development@smetj.net>
2014-06-06 00:50:40 +0200

    Initial commit of complete rewrite

--- wishbone/module/msgpack.py
+++ wishbone/module/msgpack.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-#
 # -*- coding: utf-8 -*-
 #
-#  loglevelfilter.py
+#  msgpack.py
 #
-#  Copyright 2013 Jelle Smet <development@smetj.net>
+#  Copyright 2014 Jelle Smet <development@smetj.net>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -22,27 +21,3 @@
 #  MA 02110-1301, USA.
 #
 #
-
-from wishbone import Actor
-from time import strftime, localtime
-from time import time
-
-
-class LogLevelFilter(Actor):
-    '''**Filters Wishbone log events.**
-
-        name(str)       :   The name of the module.
-
-        max_level(bool) :   The maximum log level to show.
-                            Default: 6
-    '''
-
-    def __init__(self, name, max_level=6):
-        Actor.__init__(self, name)
-        self.name=name
-        self.max_level=max_level
-
-    def consume(self, event):
-
-        if event["data"][0] <= self.max_level:
-            self.queuepool.outbox.put(event)
