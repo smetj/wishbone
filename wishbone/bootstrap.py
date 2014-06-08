eb89d1f09d4ef53ef8361a7b550b628765bb3453
Jelle Smet <development@smetj.net>
2014-06-08 14:32:35 +0200

    Fleshing out, cleaning up

--- wishbone/bootstrap.py
+++ wishbone/bootstrap.py
@@ -26,138 +26,14 @@ import argparse
 import pkg_resources
 import sys
 import yaml
-from gevent import sleep, signal
+import daemon
+from gevent import signal
 from wishbone.router import Default
 
-class Module():
-
-    def __init__(self):
-        pass
-
-    def extractSummary(self, entrypoint):
-        try:
-            doc=entrypoint.load().__doc__
-        except Exception as err:
-            return "! -> Unable to load.  Reason: %s"%(err)
-        try:
-            return re.search('.*?\*\*(.*?)\*\*', doc, re.DOTALL ).group(1)
-        except:
-            return "No description found."
-
-    def getVersion(self, entrypoint):
-        modulename = vars(entrypoint)["module_name"].split('.')[0]
-
-        try:
-            return pkg_resources.get_distribution(modulename).version
-        except Exception as err:
-            return "Unknown"
-
-    def load(self, entrypoint):
-        '''Loads a module from an entrypoint string and returns it.'''
-
-        e=entrypoint.split('.')
-        name=e[-1]
-        del(e[-1])
-        group=".".join(e)
-        module_instance=None
-
-        for module in pkg_resources.iter_entry_points(group=group, name=name):
-            try:
-                module_instance=module.load()
-            except Exception as err:
-                raise Exception ("Problem loading module %s  Reason: %s"%(module, err))
-
-        if module_instance != None:
-            return module_instance
-        else:
-            raise Exception ("Failed to load module %s  Reason: Not found"%(entrypoint))
-
-class Config():
-
-    def __init__(self):
-        pass
-
-    def load(self, filename):
-
-        try:
-            with open (filename, 'r') as f:
-                return yaml.load(f)
-        except Exception as err:
-            print "Failed to load config file.  Reason: %s"%(err)
-            sys.exit(1)
-
-class Dispatch():
-
-    def __init__(self):
-        signal(2, self.stop)
-        self.module = Module()
-        self.config = Config()
-
-    def block(self):
-
-        while True:
-            sleep(1)
-
-    def debug(self, command, config, instances):
-
-        config = self.config.load(config)
-        self.router = Default()
-        self.setupModules(config["modules"])
-        self.setupRoutes(config["routingtable"])
-
-
-        #In debug mode we write our logs to STDOUT
-        log_stdout = self.loadModule("wishbone.output.stdout")
-        log_human = self.loadModule("wishbone.logging.humanlogformat")
-        self.router.initializeModule(log_stdout, "log_stdout")
-        self.router.initializeModule(log_human, "log_format")
-        self.router.pool.getModule("logs_funnel").connect("outbox", self.router.pool.getModule("log_format"), "inbox")
-        self.router.pool.getModule("log_format").connect("outbox", self.router.pool.getModule("log_stdout"), "inbox")
-
-        self.router.start()
-        self.block()
-
-    def loadModule(self, name):
-
-        return self.module.load(name)
-
-    def setupModules(self, modules):
-        '''Loads and initialzes the modules from the bootstrap file.'''
-
-        for module in modules:
-            m = self.loadModule(modules[module]["module"])
-            if modules[module].has_key("arguments"):
-                self.router.initializeModule(m, module, **modules[module]["arguments"])
-            else:
-                self.router.initializeModule(m, module)
-
-    def setupRoutes(self, table):
-        '''Connects the modules from the bootstrap file.'''
-
-        for route in table:
-            sm, sq, dm, dq = self.splitRoute(route)
-            self.router.pool.getModule(sm).connect(sq, self.router.pool.getModule(dm), dq)
-
-    def splitRoute(self, definition):
-        '''Splits the route definition into 4 separate parts.'''
-
-        (source, destination) = definition.split('->')
-        (sm, sq) = source.rstrip().lstrip().split('.')
-        (dm, dq) = destination.rstrip().lstrip().split('.')
-        return sm, sq, dm, dq
-
-    def start(self):
-
-        self.router.start()
-
-    def stop(self):
-
-        self.router.stop()
-        sys.exit(0)
-
 class BootStrap():
+    '''Bootstraps a Wishbone instance.'''
 
-    def __init__(self, description="Wishbone bootstrap server."):
+    def __init__(self, description="Wishbone bootstrap server. Building async event pipeline servers."):
 
         parser = argparse.ArgumentParser(description=description)
         subparsers = parser.add_subparsers(dest='command')
@@ -187,6 +63,169 @@ class BootStrap():
 
         dispatch=Dispatch()
         getattr(dispatch, arguments["command"])(**arguments)
+
+class Config():
+    '''Handles bootstrap file interaction'''
+
+    def __init__(self):
+        pass
+
+    def load(self, filename):
+        '''Loads and returns the yaml bootstrap file.'''
+
+        try:
+            with open (filename, 'r') as f:
+                return yaml.load(f)
+        except Exception as err:
+            print "Failed to load config file.  Reason: %s"%(err)
+            sys.exit(1)
+
+class Dispatch():
+    '''Handles the Wishbone instance commands.'''
+
+    def __init__(self):
+        signal(2, self.stop)
+        self.config = Config()
+        self.instances = []
+
+    def debug(self, command, config, instances):
+        '''Handles the Wishbone debug command.'''
+
+        config = self.config.load(config)
+
+        if instances == 1:
+            self.instances.append(RouterProcess(config, debug=True, daemon=False))
+            self.instances[-1].start()
+
+    def start(self, command, config, instances):
+        '''Handles the Wishbone start command.'''
+
+        self.router.start()
+
+    def stop(self):
+        '''Hanles the Wishbone stop command.'''
+
+        for instance in self.instances:
+            instance.stop()
+        sys.exit(0)
+
+class Module():
+    '''Handles all Wishbone module interaction.'''
+
+    def __init__(self):
+        pass
+
+    def extractSummary(self, entrypoint):
+        '''Extracts and returns a module's docstring using the entrypoint'''
+
+        try:
+            doc=entrypoint.load().__doc__
+        except Exception as err:
+            return "! -> Unable to load.  Reason: %s"%(err)
+        try:
+            return re.search('.*?\*\*(.*?)\*\*', doc, re.DOTALL ).group(1)
+        except:
+            return "No description found."
+
+    def getVersion(self, entrypoint):
+        '''Extracts and returns a module's version.'''
+
+        modulename = vars(entrypoint)["module_name"].split('.')[0]
+
+        try:
+            return pkg_resources.get_distribution(modulename).version
+        except Exception as err:
+            return "Unknown"
+
+    def load(self, entrypoint):
+        '''Loads a module from an entrypoint string and returns it.'''
+
+        e=entrypoint.split('.')
+        name=e[-1]
+        del(e[-1])
+        group=".".join(e)
+        module_instance=None
+
+        for module in pkg_resources.iter_entry_points(group=group, name=name):
+            try:
+                module_instance=module.load()
+            except Exception as err:
+                raise Exception ("Problem loading module %s  Reason: %s"%(module, err))
+
+        if module_instance != None:
+            return module_instance
+        else:
+            raise Exception ("Failed to load module %s  Reason: Not found"%(entrypoint))
+
+class RouterProcess():
+    '''Setup, configure, run and optionally daemonize a router process.'''
+
+    def __init__(self, config, debug=False, daemon=True):
+        self.config = config
+        self.debug = debug
+        self.daemon = daemon
+        self.router = Default()
+        self.module = Module()
+        self.setupModules(config["modules"])
+        self.setupRoutes(config["routingtable"])
+
+        if debug == True:
+            self.__debug()
+
+    def loadModule(self, name):
+        '''Loads a module using the entrypoint name.'''
+
+        return self.module.load(name)
+
+    def setupModules(self, modules):
+        '''Loads and initialzes the modules from the bootstrap file.'''
+
+        for module in modules:
+            m = self.loadModule(modules[module]["module"])
+            if modules[module].has_key("arguments"):
+                self.router.initializeModule(m, module, **modules[module]["arguments"])
+            else:
+                self.router.initializeModule(m, module)
+
+    def setupRoutes(self, table):
+        '''Connects the modules from the bootstrap file.'''
+
+        for route in table:
+            sm, sq, dm, dq = self.__splitRoute(route)
+            self.router.pool.getModule(sm).connect(sq, self.router.pool.getModule(dm), dq)
+
+    def start(self):
+        '''Calls the router's start() function.
+        When required daemonizex
+        '''
+
+        if self.daemon == False:
+            self.router.start()
+            self.router.block()
+
+    def stop(self):
+        '''Calls the router's stop() function.'''
+
+        self.router.stop()
+
+    def __debug(self):
+        '''In debug mode we route all logging to SDOUT.'''
+
+        #In debug mode we write our logs to STDOUT
+        log_stdout = self.loadModule("wishbone.output.stdout")
+        log_human = self.loadModule("wishbone.logging.humanlogformat")
+        self.router.initializeModule(log_stdout, "log_stdout")
+        self.router.initializeModule(log_human, "log_format")
+        self.router.pool.getModule("logs_funnel").connect("outbox", self.router.pool.getModule("log_format"), "inbox")
+        self.router.pool.getModule("log_format").connect("outbox", self.router.pool.getModule("log_stdout"), "inbox")
+
+    def __splitRoute(self, definition):
+        '''Splits the route definition into 4 separate parts.'''
+
+        (source, destination) = definition.split('->')
+        (sm, sq) = source.rstrip().lstrip().split('.')
+        (dm, dq) = destination.rstrip().lstrip().split('.')
+        return sm, sq, dm, dq
 
 def main():
     bootstrap = BootStrap()
