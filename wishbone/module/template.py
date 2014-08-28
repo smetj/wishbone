bbb27cd8212d672967b20d059a2f814c0a0c7639
Jelle Smet <development@smetj.net>
2014-08-29 00:12:41 +0200

    pyflakes

--- wishbone/module/template.py
+++ wishbone/module/template.py
@@ -23,10 +23,8 @@
 #
 #
 
-from wishbone.error import QueueFull, QueueLocked
 from jinja2 import Environment, FileSystemLoader
 from jinja2 import Template as JinjaTemplate
-from gevent import spawn
 from wishbone import Actor
 
 
