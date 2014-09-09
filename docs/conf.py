94f8a2d372a409dc446451c911e3625bfbc38018
Jelle Smet <development@smetj.net>
2014-09-09 03:21:12 +0200

    expanding mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ MOCK_MODULES = ['gevent', 'argparse', 'greenlet', 'jsonschema', 'prettytable',
                 "amqp", "amqp.connection", "gevent.hub", "gevent.wsgi", "gevent.queue",
                 "werkzeug.exceptions", "werkzeug", "abort", "redirect", "werkzeug.utils", "utils",
                 "werkzeug.datastructures", "ImmutableDict", "werkzeug.routing", "Map",
-                "Rule", "RequestRedirect", "BuildError", "urlparse", "quote", "url_quote"]
+                "Rule", "RequestRedirect", "BuildError", "urlparse", "quote", "url_quote",
+                "werkzeug.local", "LocalStack", "LocalProxy"]
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
