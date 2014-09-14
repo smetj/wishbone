bcdd94c31b6324ad53f2a3f6a3b9905a62832f98
Jelle Smet <development@smetj.net>
2014-09-14 22:03:57 +0200

    yet another mock config build test

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ MOCK_MODULES = ["gevent",  "gevent.lock", "gevent.pool", "gevent.fileobject", "g
                 "werkzeug.exceptions", "werkzeug", "abort", "redirect", "werkzeug.utils", "utils",
                 "werkzeug.datastructures", "ImmutableDict", "werkzeug.routing", "Map",
                 "Rule", "RequestRedirect", "BuildError", "urlparse", "quote", "url_quote",
-                "werkzeug.local", "LocalStack", "LocalProxy", "werkzeug.exceptions"]
+                "werkzeug.local", "LocalStack", "LocalProxy", "werkzeug.exceptions", "flask.globals"]
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
