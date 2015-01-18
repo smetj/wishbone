40a0166444a7c9ebc00e15fd9956e174f013fbfd
Jelle Smet <development@smetj.net>
2015-01-18 14:26:36 +0100

    remove non recursive function

--- wishbone/configurationfactory.py
+++ wishbone/configurationfactory.py
@@ -186,20 +186,6 @@ class ConfigurationFactory(object):
 
         return do(args)
 
-    # def replaceLookupDefsWithFunctions(self, args):
-
-    #     for arg in args:
-    #         if isinstance(args[arg], str) and args[arg].startswith('~'):
-    #             (t, lookup, var) = self.extractLookupDef(args[arg])
-    #             try:
-    #                 if t == 'dynamic':
-    #                     args[arg] = Lookup(self.lookup_methods[lookup].generateLookup(var))
-    #                 else:
-    #                     args[arg] = self.lookup_methods[lookup].generateLookup(var)()
-    #             except KeyError:
-    #                 raise ModuleInitFailure('"%s" is an unknown lookup instance name.' % (lookup))
-
-    #     return args
 
     def extractLookupDef(self, e):
 
