1678a5b5007db1c3d8509941a88a5cc3dcd14fae
Jelle Smet <development@smetj.net>
2015-04-26 21:10:56 +0200

    more mockery

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ MOCK_MODULES = ["gevent",  "gevent.lock", "gevent.pool", "gevent.fileobject", "g
                 "gevent.pool", "wishbone.tools", "gevent.fileobject", "gevent.server",
                 "amqp", "amqp.connection", "gevent.hub", "gevent.wsgi", "gevent.queue",
                 "flask", "ReadRulesDisk", "gevent_inotifyx", "amqp.exceptions", "Event",
-                "uplook", "UpLook", "msgpack", "zmq", "zmq.green"]
+                "uplook", "UpLook", "msgpack", "zmq", "zmq.green", "grequests"]
 
 
 for mod_name in MOCK_MODULES:
