e0342341217126bcf80b9ebfb0ceb1e2148e8a1f
Jelle Smet <development@smetj.net>
2015-09-21 22:27:03 +0200

    New tests for dictgenerator module

---
+++ tests/test_module_dictgenerator.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test_module_dictgenerator.py
+#
+#  Copyright 2015 Jelle Smet <development@smetj.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+from wishbone.module.dictgenerator import DictGenerator
+from wishbone.actor import ActorConfig
+from utils import getter
+
+
+def test_module_dictgenerator_keys():
+
+    actor_config = ActorConfig('template', 100, 1, {})
+    dictgenerator = DictGenerator(actor_config, keys=["one", "two"])
+
+    dictgenerator.pool.queue.outbox.disableFallThrough()
+    dictgenerator.start()
+
+    event = getter(dictgenerator.pool.queue.outbox)
+
+    assert "one" in event.data.keys()
+    assert "two" in event.data.keys()
+
+def test_module_dictgenerator_randomize_keys():
+
+    actor_config = ActorConfig('template', 100, 1, {})
+    dictgenerator = DictGenerator(actor_config, randomize_keys=False)
+
+    dictgenerator.pool.queue.outbox.disableFallThrough()
+    dictgenerator.start()
+
+    event = getter(dictgenerator.pool.queue.outbox)
+
+    assert '0' in event.data.keys()
+
+def test_module_dictgenerator_num_values():
+
+    actor_config = ActorConfig('template', 100, 1, {})
+    dictgenerator = DictGenerator(actor_config, num_values=True)
+
+    dictgenerator.pool.queue.outbox.disableFallThrough()
+    dictgenerator.start()
+
+    event = getter(dictgenerator.pool.queue.outbox)
+
+    for key, value in event.data.iteritems():
+        assert isinstance(value, int)
+
+    assert isinstance(event.data.items()[0][1], int)
+
+def
