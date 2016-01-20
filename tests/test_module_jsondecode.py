ed0ab9b820b7394b3d3e755d88cdc2efcc39b2e3
Jelle Smet <development@smetj.net>
2016-01-20 22:43:53 +0100

    add deserialize option and source/destination support

--- tests/test_module_jsondecode.py
+++ tests/test_module_jsondecode.py
@@ -28,7 +28,7 @@ from wishbone.actor import ActorConfig
 from utils import getter
 
 
-def test_module_jsondecode():
+def test_module_jsondecode_basic():
 
     actor_config = ActorConfig('jsondecode', 100, 1, {}, "")
     jsondecode = JSONDecode(actor_config)
@@ -42,3 +42,23 @@ def test_module_jsondecode():
     jsondecode.pool.queue.inbox.put(e)
     one = getter(jsondecode.pool.queue.outbox)
     assert one.get() == ["one", "two", "three"]
+
+
+def test_module_jsondecode_list_to_events():
+
+    actor_config = ActorConfig('jsondecode', 100, 1, {}, "")
+    jsondecode = JSONDecode(actor_config, deserialize=True)
+
+    jsondecode.pool.queue.inbox.disableFallThrough()
+    jsondecode.pool.queue.outbox.disableFallThrough()
+    jsondecode.start()
+
+    e = Event('[{"one": 1}, {"two": 2}, {"three": 3}]')
+
+    jsondecode.pool.queue.inbox.put(e)
+    one = getter(jsondecode.pool.queue.outbox)
+    two = getter(jsondecode.pool.queue.outbox)
+    three = getter(jsondecode.pool.queue.outbox)
+    assert one.get() == {"one": 1}
+    assert two.get() == {"two": 2}
+    assert three.get() == {"three": 3}
