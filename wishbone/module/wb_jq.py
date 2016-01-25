593c2ed7e750f0ff4e19651d0553c0759d3d13ac
Jelle Smet <development@smetj.net>
2016-01-25 21:57:29 +0100

    Include disk read functionality

--- wishbone/module/wb_jq.py
+++ wishbone/module/wb_jq.py
@@ -22,6 +22,20 @@
 #
 #
 
+from wishbone import Actor
+from jq import jq
+from jsonschema import validate
+
+import os
+import yaml
+import gevent_inotifyx as inotify
+from gevent.lock import Semaphore
+from gevent import sleep
+from glob import glob
+from yaml.parser import ParserError
+
+
+
 SCHEMA = {
     "type": "object",
     "properties": {
@@ -49,11 +63,6 @@ SCHEMA = {
     "required": ["name", "expression", "queue"],
     "additionalProperties": False
 }
-
-
-from wishbone import Actor
-from jq import jq
-from jsonschema import validate
 
 
 class JQ(Actor):
@@ -115,9 +124,10 @@ class JQ(Actor):
         - conditions(dict)([])
            |  A dictionary consisting out of expression, queue, payload.
 
-        - conditions_directory(str)("./rules")
+        - location(str)("")
            |  A directory containing rules.  This directory will be monitored
            |  for changes and automatically read for changes.
+           |  An empty value disables this functionality.
 
 
     Queues:
@@ -126,36 +136,64 @@ class JQ(Actor):
            |  Incoming events.
     '''
 
-    def __init__(self, actor_config, selection="@data", conditions=[], conditions_directory="./"):
+    def __init__(self, actor_config, selection="@data", conditions=[], location=""):
         Actor.__init__(self, actor_config)
 
         self.pool.createQueue("inbox")
         self.registerConsumer(self.consume, "inbox")
         self.disk_conditions = []
+        self.condition_read_lock = Semaphore()
 
     def preHook(self):
 
         self.kwargs.conditions = self.validateConditions(self.kwargs.conditions)
 
+        if self.kwargs.location == "":
+            self.logging.info("No rules directory defined, not reading rules from disk.")
+        else:
+            self.logging.info("Rules directoy '%s' defined." % (self.kwargs.location))
+            self.sendToBackground(self.monitorRuleDirectory)
+
+    def monitorRuleDirectory(self):
+
+        '''
+        Loads new rules when changes happen.
+        '''
+
+        self.monitor_location = ReadRulesDisk(self.logging, self.kwargs.location)
+        self.disk_conditions = self.monitor_location.readDirectory()
+        self.logging.info("Read %s rules from disk and %s defined in config." % (len(self.disk_conditions), len(self.kwargs.conditions)))
+
+        while self.loop():
+            try:
+                fresh_conditions = self.monitor_location.waitForChanges()
+                with self.condition_read_lock:
+                    if cmp(self.disk_conditions, fresh_conditions) != 0:
+                        self.disk_conditions = fresh_conditions
+                        self.logging.info("Read %s rules from disk and %s defined in config." % (len(self.disk_conditions), len(self.kwargs.conditions)))
+            except Exception as err:
+                self.logging.warning("Problem reading rules directory.  Reason: %s" % (err))
+                sleep(1)
+
     def consume(self, event):
 
         data = event.get(self.kwargs.selection)
 
-        for condition in self.kwargs.conditions + self.disk_conditions:
-            if self.pool.hasQueue(condition["queue"]):
-                result = jq(condition["expression"]).transform(data)
-                if isinstance(result, bool):
-                    if result:
-                        e = event.clone()
-                        if "payload" in condition:
-                            for key, value in condition["payload"].iteritems():
-                                e.set(value, key)
-                        self.submit(e, self.pool.getQueue(condition["queue"]))
+        with self.condition_read_lock:
+            for condition in self.kwargs.conditions + self.disk_conditions:
+                if self.pool.hasQueue(condition["queue"]):
+                    result = jq(condition["expression"]).transform(data)
+                    if isinstance(result, bool):
+                        if result:
+                            e = event.clone()
+                            if "payload" in condition:
+                                for key, value in condition["payload"].iteritems():
+                                    e.set(value, key)
+                            self.submit(e, self.pool.getQueue(condition["queue"]))
+                    else:
+                        self.logging.error("Jq expression '%s' does not return a bool therefor it is skipped." % (condition["name"]))
                 else:
-                    self.logging.error("Jq expression '%s' does not return a bool therefor it is skipped." % (condition["name"]))
-            else:
-                self.logging.error("Condition '%s' has queue '%s' defined but nothing connected." % (condition["name"], condition["queue"]))
-
+                    self.logging.error("Condition '%s' has queue '%s' defined but nothing connected." % (condition["name"], condition["queue"]))
 
     def validateConditions(self, conditions):
         '''
@@ -172,3 +210,75 @@ class JQ(Actor):
             finally:
                 rules.append(condition)
         return rules
+
+
+class ReadRulesDisk():
+
+    '''
+    Loads jq rules from a directory and monitors the directory for
+    changes.
+
+    Parameters:
+
+        directory(string):   The directory to load rules from.
+                            default: rules/
+
+    '''
+
+    def __init__(self, logger, directory="rules/"):
+        self.logging = logger
+        self.directory = directory
+
+        if not os.access(self.directory, os.R_OK):
+            raise Exception("Directory '%s' is not readable. Please verify." % (self.directory))
+
+        self.fd = inotify.init()
+        self.wb = inotify.add_watch(self.fd, self.directory, inotify.IN_CLOSE_WRITE + inotify.IN_CREATE + inotify.IN_DELETE + inotify.IN_MODIFY + inotify.IN_MOVE)
+        self.logging.info("Monitoring directory '%s' for changes" % (directory))
+
+    def createDir(self):
+
+        if os.path.exists(self.kwargs.location):
+            if not os.path.isdir(self.kwargs.location):
+                raise Exception("%s exists but is not a directory" % (self.kwargs.location))
+            else:
+                self.logging.info("Directory %s exists so I'm using it." % (self.kwargs.location))
+        else:
+            self.logging.info("Directory %s does not exist so I'm creating it." % (self.kwargs.location))
+            os.makedirs(self.kwargs.location)
+
+    def readDirectory(self):
+
+        return self.__readDirectory()
+
+    def waitForChanges(self):
+
+        events = inotify.get_events(self.fd)
+        return self.readDirectory()
+
+    def __readDirectory(self):
+        '''Reads the content of the given directory and creates a dict
+        containing the rules.'''
+
+        rules = []
+        for filename in glob("%s/*.yaml" % (self.directory)):
+            try:
+                with open(filename, 'r') as f:
+                    rule_name = os.path.basename(filename).rstrip(".yaml")
+                    rule = yaml.load("".join(f.readlines()))
+                    try:
+                        validate(rule, SCHEMA)
+                    except Exception as err:
+                        self.logging.warning("Rule %s not valid. Skipped. Reason: %s" % (filename, err.message))
+                    else:
+                        rule["name"] = rule_name
+                        rules.append[rule]
+            except ParserError as err:
+                self.logging.warning("Failed to parse file %s.  Please validate the YAML syntax in a parser." % (filename))
+            except IOError as err:
+                self.logging.warning("Failed to read %s.  Reason: %s" % (filename, err))
+            except Exception as err:
+                self.logging.warning("Unknown error parsing file %s.  Skipped.  Reason: %s." % (filename, err))
+
+        return rules
+
