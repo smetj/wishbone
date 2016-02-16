af3f7ee2811cc4ef67b1934dee37da2c1f45805d
Jelle Smet <development@smetj.net>
2016-02-16 23:25:49 +0100

    stick to original version number

--- setup.py
+++ setup.py
@@ -57,18 +57,18 @@ install_requires = ['gevent==1.1rc4',
                     'elasticsearch==1.6.0',
                     'importlib==1.0.3',
                     'uplook==0.4.1',
-                    'pyjq==1.1']
+                    'pyjq==1.0']
 
 # Dirty hack to make readthedocs build the docs
 # For some reason mocking out jq as documented is not working
 
 if os.environ.get("READTHEDOCS", False):
     dependency_links = []
-    # install_requires.remove('pyjq==1.1')
+    # install_requires.remove('pyjq==1.0')
 
 else:
     dependency_links = [
-        'https://github.com/smetj/pyjq/tarball/master#egg=pyjq-1.1'
+        'https://github.com/smetj/pyjq/tarball/master#egg=pyjq-1.0'
     ]
 
 # Deps pulled in by other modules
