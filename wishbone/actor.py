9dcfd1436170b492df7708afc9fb1d77b6dc8d90
Jelle Smet <development@smetj.net>
2016-03-22 10:33:20 +0100

    method not relevant anymore

--- wishbone/actor.py
+++ wishbone/actor.py
@@ -97,14 +97,6 @@ class Actor():
         self.pool.getQueue(source).disableFallThrough()
         self.logging.debug("Connected queue %s.%s to %s.%s" % (self.name, source, destination_module.name, destination_queue))
 
-    def createEvent(self):
-
-        '''
-        Convenience function which returns an empty Wishbone event with the
-        current namespace already set.
-        '''
-
-        return Wishbone_Event()
 
     def doEventLookup(self, name):
 
