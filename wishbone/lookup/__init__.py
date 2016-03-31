e9e54e870983457318ef01b324fa5544b8190c33
Jelle Smet <jelle.smet@tomtom.com>
2016-03-31 23:20:00 +0200

    add cycle lookup function

--- wishbone/lookup/__init__.py
+++ wishbone/lookup/__init__.py
@@ -27,67 +27,11 @@ class EventLookup():
     pass
 
 
-def event():
-    '''Returns the requested event header value.
-
-    - Parameters to initialize the function:
-
-        None
-
-    - Parameters to call the function:
-
-        When calling the function a variable reference can be used similar to:
-
-        ~~headerlookup("modulename.header.variablename","unknown")
-
-        Keep in mind you always have to use a dynamic lookup function (double
-        tilde).  You can provide a default value in case <variablename> does not
-        exist in the header of namespace <modulename>.
-    '''
-
-    return EventLookup()
 
 
-def randomword(filename=None):
-    '''Returns a random word.
-
-    This function returns a random word from a wordlist.  Each line is a new
-    word.
-
-    - Parameters to initialize the function:
-
-        - filename(str)(None): When value is None, a buildin wordlist is used.
-                               If not a filename of a wordlist is expected.
 
 
-    - Parameters to call the function:
 
-        None
-    '''
-
-    import os
-    from random import choice
-
-    class RandomWord():
-
-        def __init__(self, f=None):
-            self.wordlist = self.readWordlist()
-
-        def readWordlist(self):
-            with open("%s/../data/wordlist.txt" % (os.path.dirname(__file__))) as f:
-                return f.readlines()
-
-        def pickWord(self):
-            '''Returns a word as string from the wordlist.'''
-
-            while True:
-                word = choice(self.wordlist).rstrip()
-                try:
-                    return word.encode("ascii", "ignore")
-                except:
-                    pass
-
-    return RandomWord(filename).pickWord
 
 
 def choice(values):
@@ -140,6 +84,45 @@ def cycle(values):
     return lookup
 
 
+def etcd(base="http://127.0.0.1:2379/v2/keys"):
+
+    import requests
+
+    base = base.rstrip('/')
+
+    def etcdLookup(key):
+        key = key.lstrip('/')
+
+        try:
+            response = requests.get('%s/%s' % (base, key))
+            response.raise_for_status()
+            return response.json()["node"]["value"]
+        except Exception as err:
+            raise NoSuchValue(str(err))
+    return etcdLookup
+
+
+def event():
+    '''Returns the requested event header value.
+
+    - Parameters to initialize the function:
+
+        None
+
+    - Parameters to call the function:
+
+        When calling the function a variable reference can be used similar to:
+
+        ~~headerlookup("modulename.header.variablename","unknown")
+
+        Keep in mind you always have to use a dynamic lookup function (double
+        tilde).  You can provide a default value in case <variablename> does not
+        exist in the header of namespace <modulename>.
+    '''
+
+    return EventLookup()
+
+
 def randombool():
     '''Returns True or False.
 
@@ -184,22 +167,46 @@ def randominteger(min=0, max=100):
     return randomInteger
 
 
-def etcd(base="http://127.0.0.1:2379/v2/keys"):
+def randomword(filename=None):
+    '''Returns a random word.
 
-    import requests
+    This function returns a random word from a wordlist.  Each line is a new
+    word.
 
-    base = base.rstrip('/')
+    - Parameters to initialize the function:
 
-    def etcdLookup(key):
-        key = key.lstrip('/')
+        - filename(str)(None): When value is None, a buildin wordlist is used.
+                               If not a filename of a wordlist is expected.
 
-        try:
-            response = requests.get('%s/%s' % (base, key))
-            response.raise_for_status()
-            return response.json()["node"]["value"]
-        except Exception as err:
-            raise NoSuchValue(str(err))
-    return etcdLookup
+
+    - Parameters to call the function:
+
+        None
+    '''
+
+    import os
+    from random import choice
+
+    class RandomWord():
+
+        def __init__(self, f=None):
+            self.wordlist = self.readWordlist()
+
+        def readWordlist(self):
+            with open("%s/../data/wordlist.txt" % (os.path.dirname(__file__))) as f:
+                return f.readlines()
+
+        def pickWord(self):
+            '''Returns a word as string from the wordlist.'''
+
+            while True:
+                word = choice(self.wordlist).rstrip()
+                try:
+                    return word.encode("ascii", "ignore")
+                except:
+                    pass
+
+    return RandomWord(filename).pickWord
 
 
 def uuid():
