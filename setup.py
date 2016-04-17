38846a7ecce42876fc9b4d4bf36ff57cb6decbf7
Jelle Smet <jelle.smet@tomtom.com>
2016-04-17 16:42:07 +0200

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'wishbone'
-VERSION = '2.1.4'
+VERSION = '2.1.5'
 
 install_requires = ['gevent==1.1.1',
                     'greenlet==0.4.9',
