a6adc7452063fbbc5ea729d0d6477ff89b0c5c06
Jelle Smet <development@smetj.net>
2016-11-30 22:02:41 +0100

    renamed parameter to ack_id and made sure it has a default EventLookup value.

--- wishbone/module/acknowledge.py
+++ wishbone/module/acknowledge.py
@@ -24,6 +24,9 @@
 
 from wishbone import Actor
 from gevent.lock import Semaphore
+from wishbone.lookup import EventLookup
+from random import SystemRandom
+import string
 
 class AckList(object):
 
@@ -56,14 +59,14 @@ class Acknowledge(Actor):
 
     '''**Lets events pass or not based on some event value present or not in a lookup table.**
 
-    This module stores a value <ack_value> from passing events in a list and
-    only let's events go through for which the <ack_value> value is not in the
+    This module stores a value <ack_id> from passing events in a list and
+    only let's events go through for which the <ack_id> value is not in the
     list.
 
-    <ack_value> can be removed from the list by sending the event into the
+    <ack_id> can be removed from the list by sending the event into the
     <acknowledge> queue.
 
-    <ack_value> should some unique identifier to make sure that any following
+    <ack_id> should some unique identifier to make sure that any following
     <modules are not processing events with the same datastructure.
 
     Typically, downstream modules's <successful> and/or <failed> queues are
@@ -71,9 +74,10 @@ class Acknowledge(Actor):
 
     Parameters:
 
-        - ack_value(str)(None)*
-           |  The key
-
+        - ack_id(EventLookup("@data"))
+           |  A value stored somewhere in the event which then acts as the
+           |  ack_id. It possibly makes only sense to define an EventLookup
+           |  value here.
 
     Queues:
 
@@ -91,9 +95,15 @@ class Acknowledge(Actor):
 
     '''
 
-    def __init__(self, actor_config, ack_value=None):
+    def __init__(self, actor_config, ack_id=None):
+
+        if ack_id is None:
+            event_lookup_string = ''.join(SystemRandom().choice(string.ascii_lowercase) for _ in range(4))
+            actor_config.lookup[event_lookup_string] = EventLookup()
+            ack_id = "%s('@data')" % (event_lookup_string)
+
         Actor.__init__(self, actor_config)
-        self.ack_value_ref = ack_value
+        self.ack_id_ref = ack_id
 
         self.pool.createQueue("inbox")
         self.pool.createQueue("outbox")
@@ -105,24 +115,24 @@ class Acknowledge(Actor):
 
     def consume(self, event):
 
-        if self.kwargs.ack_value is None:
-            self.logging.warning("Incoming event with <ack_value> %s does not seem to exist.  Returns a None value. Event passing through." % (self.ack_value_ref))
+        if self.kwargs.ack_id is None:
+            self.logging.warning("Incoming event with <ack_id> %s does not seem to exist.  Returns a None value. Event passing through." % (self.ack_id_ref))
             self.pool.queue.outbox.put(event)
-        elif self.ack_table.unack(self.kwargs.ack_value):
-            self.logging.debug("Event unacknowledged with <ack_value> '%s'." % (self.kwargs.ack_value))
+        elif self.ack_table.unack(self.kwargs.ack_id):
+            self.logging.debug("Event unacknowledged with <ack_id> '%s'." % (self.kwargs.ack_id))
             self.pool.queue.outbox.put(event)
         else:
-            self.logging.debug("Event with still unacknowledged <ack_value> '%s' send to <dropped> queue." % (self.kwargs.ack_value))
+            self.logging.debug("Event with still unacknowledged <ack_id> '%s' send to <dropped> queue." % (self.kwargs.ack_id))
             self.pool.queue.dropped.put(event)
 
 
     def acknowledge(self, event):
-        if self.kwargs.ack_value is None:
-            self.logging.warning("Incoming acknowledge event with <ack_value> %s does not seem to exist.  Returns a None value. Nothing to do." % (self.ack_value_ref))
-        elif self.ack_table.ack(self.kwargs.ack_value):
-            self.logging.debug("Event acknowledged with <ack_value> '%s'." % (self.kwargs.ack_value))
+        if self.kwargs.ack_id is None:
+            self.logging.warning("Incoming acknowledge event with <ack_id> %s does not seem to exist.  Returns a None value. Nothing to do." % (self.ack_id_ref))
+        elif self.ack_table.ack(self.kwargs.ack_id):
+            self.logging.debug("Event acknowledged with <ack_id> '%s'." % (self.kwargs.ack_id))
         else:
-            self.logging.debug("Event with <ack_value> '%s' received but was not previously acknowledged." % (self.kwargs.ack_value))
+            self.logging.debug("Event with <ack_id> '%s' received but was not previously acknowledged." % (self.kwargs.ack_id))
 
     def postHook(self):
 
